82eca41ecb89ddceab753bca9233d1962bb7693f
matt joseloff <msjo425@gmail.com>
2020-04-16 17:24:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,5 +4,5 @@ var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 function addElementToBeginningOfArray(arrayName,newElement){
 newArray = arrayName.unshift(newElement)
 return newArray
-newArray = [newElement]
+newArray = [newElement, ...arrayName]
 }
