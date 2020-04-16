466cf3151b3ad292809180df9ed37f19958029e9
matt joseloff <msjo425@gmail.com>
2020-04-16 18:00:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,6 +38,3 @@ function removeElementFromEndOfArray(arrayName){
   newArray = arrayName.slice(0, arrayName.length-1)
   return newArray
 }
-function destructivelyRemoveElementFromEndOfArray(arrayName){
-  
-}
